6bd966d89858aac4eab4708ca72418724ca8d8fc
tpo-dburbine <77117608+tpo-dburbine@users.noreply.github.com>
2021-01-25 14:03:02 -0500

    Update script.js
    working event listeners for job role and shirt colors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
+// set focus on text field
+const focusText = document.querySelector('#name')
+focusText.focus()
 
+// -----------------------------------------------------------------------------------------------------------------
+// GLOBAL VARIABLES
+// -----------------------------------------------------------------------------------------------------------------
+
+const jobRole = document.querySelector('#title')
+const otherJobRole = document.querySelector('#other-job-role')
+const shirtDesign = document.querySelector('#design')
+const shirtColor = document.querySelector('#shirt-colors')
+const colorOptions = document.querySelector('#color')
+otherJobRole.style.display = 'none'
+shirtColor.style.display = 'none'
+
+// -----------------------------------------------------------------------------------------------------------------
+// EVENT LISTENERS
+// -----------------------------------------------------------------------------------------------------------------
+
+// event listener: if job selection is 'other'
+jobRole.addEventListener('change', (e) => {
+  if (e.target.value === 'other') {
+    otherJobRole.style.display = 'block'
+  } else {
+    otherJobRole.style.display = 'none'
+  }
+})
+
+shirtDesign.addEventListener('change', (e) => {
+  shirtColor.style.display = 'block'
+
+  if (e.target.value === 'heart js') {
+    for (let i = 0; i < colorOptions.length; i++) {
+      const theme = colorOptions[i].getAttribute('data-theme')
+
+      if (theme === e.target.value) {
+        colorOptions[i].style.display = 'block'
+        colorOptions[i].selected = true
+      } else {
+        colorOptions[i].style.display = 'none'
+      }
+    }
+  } else if (e.target.value === 'js puns') {
+    for (let i = 0; colorOptions.length; i++) {
+      const theme = colorOptions[i].getAttribute('data-theme')
+
+      if (theme === e.target.value) {
+        colorOptions[i].style.display = 'block'
+        colorOptions[i].selected = true
+      } else {
+        colorOptions[i].style.display = 'none'
+      }
+    }
+  }
+})
