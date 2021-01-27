930219625824305a68babdf435462efc3d46c350
tpo-dburbine <77117608+tpo-dburbine@users.noreply.github.com>
2021-01-27 15:18:29 -0500

    working form
    moved submit listener contents into seperate functions. added functioning dynamic validity checker for email field.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tpo-dburbine-project-3
+
+
+## Conditional Error Message
+
+I added the conditional error message to the email form. When the user leaves the email field blank the message 'Oops! Email address field was left blank.' is shown and if the user inputs something that does not match the proper email format, they receive the error message 'Oops! Email address is incomplete/must be formatted properly.' To accomplish this, inside the function I used to validate the email there is a conditional nested inside the if statement testing if the regex is not valid that tests if the email address value is blank. If it is then the first messsage will display alerting the user the field is blank, the else portion alerts the user the email must be formatted properly
+
+## Real Time Validation
+
+I also added the real time validation to the email field. When the user clicks into the field the message 'Oops! Email address field was left blank.' will be shown to the user. As soon as they start typing out their email address the message will change to 'Oops! Email address is incomplete/must be formatted properly.' To accomplish this, I created another event listener on the email address that listened for keyups and called the function to check email validity when triggered.
--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 // set focus on text field
-const nameField = document.querySelector('#name')
-nameField.focus()
+document.querySelector('#name').focus()
 
 // -----------------------------------------------------------------------------------------------------------------
 // GLOBAL VARIABLES
 // -----------------------------------------------------------------------------------------------------------------
-
+const nameField = document.querySelector('#name')
 const form = document.querySelector('form')
 const jobRole = document.querySelector('#title')
 const otherJobRole = document.querySelector('#other-job-role')
 const shirtDesign = document.querySelector('#design')
-// const shirtColor = document.querySelector('#shirt-colors')
 const colorOptions = document.querySelector('#color')
 const registerForActivities = document.querySelector('#activities')
 const checkboxes = document.querySelectorAll('input[type=checkbox]')
@@ -25,12 +23,141 @@ const bitcoin = document.querySelector('#bitcoin')
 const emailAddress = document.querySelector('#email')
 
 let sumOfCost = 0
+
 otherJobRole.style.display = 'none'
 colorOptions.disabled = true
+payType.children[1].selected = true
 paypal.style.display = 'none'
 bitcoin.style.display = 'none'
 
-payType.children[1].selected = true
+// -----------------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------------------------------------------------------------------------
+
+// check for validity of name
+function validName () {
+  const nameInput = nameField.value
+  const regName = /^[A-Za-z]+ ?[A-Za-z]*?$/.test(nameInput)
+  const nameAlert = document.querySelector('#name-hint')
+
+  if (!regName) {
+    nameAlert.parentElement.classList.remove('valid')
+    nameAlert.parentElement.classList.add('not-valid')
+    nameAlert.classList.remove('hint')
+  } else if (regName) {
+    nameAlert.classList.remove('not-valid')
+    nameAlert.classList.add('valid')
+    nameAlert.classList.add('hint')
+  }
+  return regName
+}
+
+// check for validity of email field (conditional added for extra credit)
+function validEmail () {
+  const emailInput = emailAddress.value
+  const regEmail = /[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput)
+  const emailAlert = document.querySelector('#email-hint')
+
+  if (!regEmail) {
+    if (emailInput === '') {
+      emailAlert.textContent = 'Oops! Email address field was left blank.'
+    } else {
+      emailAlert.textContent = 'Oops! Email address is incomplete/must be formatted properly.'
+    }
+    emailAlert.parentElement.classList.remove('valid')
+    emailAlert.parentElement.classList.add('not-valid')
+    emailAlert.classList.remove('hint')
+  } else if (regEmail) {
+    emailAlert.parentElement.classList.remove('not-valid')
+    emailAlert.parentElement.classList.add('valid')
+    emailAlert.classList.add('hint')
+  }
+  return regEmail
+}
+
+// check validity of activities
+function validActivities () {
+  let checkedNum = 0
+  let activitiesValid
+  const activitiesAlert = document.querySelector('#activities-hint')
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkedNum++
+    }
+  }
+
+  if (checkedNum > 0) {
+    activitiesValid = true
+  } else {
+    activitiesValid = false
+  }
+
+  if (!activitiesValid) {
+    activitiesAlert.parentElement.classList.remove('valid')
+    activitiesAlert.parentElement.classList.add('not-valid')
+    activitiesAlert.classList.remove('hint')
+  } else if (activitiesValid) {
+    activitiesAlert.parentElement.classList.remove('not-valid')
+    activitiesAlert.parentElement.classList.add('valid')
+    activitiesAlert.classList.add('hint')
+  }
+  return activitiesValid
+}
+
+// check for validity of CC number
+function validCCNumber () {
+  const creditNumInput = creditCardNumber.value
+  const regCreditNum = /^\d{13}\d?\d?\d?$/.test(creditNumInput)
+  const cardNumAlert = document.querySelector('#cc-hint')
+
+  if (!regCreditNum) {
+    cardNumAlert.parentNode.classList.remove('valid')
+    cardNumAlert.parentNode.classList.add('not-valid')
+    cardNumAlert.classList.remove('hint')
+  } else if (regCreditNum) {
+    cardNumAlert.parentNode.classList.remove('not-valid')
+    cardNumAlert.parentNode.classList.add('valid')
+    cardNumAlert.classList.add('hint')
+  }
+  return regCreditNum
+}
+
+// check for validity of zip code
+function validZipCode () {
+  const zipCodeInput = zipCode.value
+  const regZipCode = /^\d{5}$/.test(zipCodeInput)
+  const zipCodeAlert = document.querySelector('#zip-hint')
+
+  if (!regZipCode) {
+    zipCodeAlert.parentNode.classList.remove('valid')
+    zipCodeAlert.parentNode.classList.add('not-valid')
+    zipCodeAlert.classList.remove('hint')
+  } else if (regZipCode) {
+    zipCodeAlert.parentNode.classList.remove('not-valid')
+    zipCodeAlert.parentNode.classList.add('valid')
+    zipCodeAlert.classList.add('hint')
+  }
+  return regZipCode
+}
+
+// check for validity of CVV number
+function validCVV () {
+  const cvvInput = cvv.value
+  const regCVV = /^\d{3}$/.test(cvvInput)
+  const cvvAlert = document.querySelector('#cvv-hint')
+
+  if (!regCVV) {
+    cvvAlert.parentNode.classList.remove('valid')
+    cvvAlert.parentNode.classList.add('not-valid')
+    cvvAlert.classList.remove('hint')
+  } else if (regCVV) {
+    cvvAlert.parentNode.classList.remove('not-valid')
+    cvvAlert.parentNode.classList.add('valid')
+    cvvAlert.classList.add('hint')
+  }
+  return regCVV
+}
 
 // -----------------------------------------------------------------------------------------------------------------
 // EVENT LISTENERS
@@ -45,6 +172,7 @@ jobRole.addEventListener('change', (e) => {
   }
 })
 
+// event listener to show specific color options for shirt design chosen
 shirtDesign.addEventListener('change', (e) => {
   colorOptions.disabled = false
 
@@ -73,6 +201,8 @@ shirtDesign.addEventListener('change', (e) => {
   }
 })
 
+// event listener to process activities chosen and add price to total cost/compare times
+// and disable if other checkboxes if they conflict with chosen activity
 registerForActivities.addEventListener('change', (e) => {
   const clickedActivity = e.target
   const activityCost = +clickedActivity.getAttribute('data-cost')
@@ -102,6 +232,7 @@ registerForActivities.addEventListener('change', (e) => {
   totalCost.innerHTML = `Total: $${sumOfCost}`
 })
 
+//  event listener to remove pay methods based on which is selected
 payType.addEventListener('change', (e) => {
   const paymentInput = e.target.value
 
@@ -120,111 +251,7 @@ payType.addEventListener('change', (e) => {
   }
 })
 
-form.addEventListener('submit', (e) => {
-  const nameInput = nameField.value
-  const emailInput = emailAddress.value
-  const creditNumInput = creditCardNumber.value
-  const zipCodeInput = zipCode.value
-  const cvvInput = cvv.value
-
-  const regName = /^[A-Za-z]+ ?[A-Za-z]*?$/.test(nameInput)
-  const regEmail = /[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput)
-  const regCreditNum = /^\d{13}\d?\d?\d?$/.test(creditNumInput)
-  const regZipCode = /^\d{5}$/.test(zipCodeInput)
-  const regCVV = /^\d{3}$/.test(cvvInput)
-
-  let checkedNum = 0
-  let activitiesValid
-
-  const nameAlert = document.querySelector('#name-hint')
-  const emailAlert = document.querySelector('#email-hint')
-  const activitiesAlert = document.querySelector('#activities-hint')
-  const cardNumAlert = document.querySelector('#cc-hint')
-  const zipCodeAlert = document.querySelector('#zip-hint')
-  const cvvAlert = document.querySelector('#cvv-hint')
-
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checkedNum++
-    }
-  }
-
-  if (checkedNum > 0) {
-    activitiesValid = true
-  } else {
-    activitiesValid = false
-  }
-
-  if (payType.value === 'credit-card') {
-    if (!regCreditNum) {
-      cardNumAlert.parentNode.classList.remove('valid')
-      cardNumAlert.parentNode.classList.add('not-valid')
-      cardNumAlert.classList.remove('hint')
-      e.preventDefault()
-    } else if (regCreditNum) {
-      cardNumAlert.parentNode.classList.remove('not-valid')
-      cardNumAlert.parentNode.classList.add('valid')
-      cardNumAlert.classList.add('hint')
-    }
-    if (!regZipCode) {
-      zipCodeAlert.parentNode.classList.remove('valid')
-      zipCodeAlert.parentNode.classList.add('not-valid')
-      zipCodeAlert.classList.remove('hint')
-      e.preventDefault()
-    } else if (regZipCode) {
-      zipCodeAlert.parentNode.classList.remove('not-valid')
-      zipCodeAlert.parentNode.classList.add('valid')
-      zipCodeAlert.classList.add('hint')
-    }
-    if (!regCVV) {
-      cvvAlert.parentNode.classList.remove('valid')
-      cvvAlert.parentNode.classList.add('not-valid')
-      cvvAlert.classList.remove('hint')
-      e.preventDefault()
-    } else if (regCVV) {
-      cvvAlert.parentNode.classList.remove('not-valid')
-      cvvAlert.parentNode.classList.add('valid')
-      cvvAlert.classList.add('hint')
-    }
-  }
-
-  if (!regName) {
-    nameAlert.parentElement.classList.remove('valid')
-    nameAlert.parentElement.classList.add('not-valid')
-    nameAlert.classList.remove('hint')
-    e.preventDefault()
-  } else if (regName) {
-    nameAlert.classList.remove('not-valid')
-    nameAlert.classList.add('valid')
-    nameAlert.classList.add('hint')
-  }
-  if (!regEmail) {
-    if (emailInput === '') {
-      emailAlert.textContent = 'Oops! Email address field was left blank.'
-    } else {
-      emailAlert.textContent = 'Oops! Email address in incomplete/must be formatted properly.'
-    }
-    emailAlert.parentElement.classList.remove('valid')
-    emailAlert.parentElement.classList.add('not-valid')
-    emailAlert.classList.remove('hint')
-    e.preventDefault()
-  } else if (regEmail) {
-    emailAlert.parentElement.classList.remove('not-valid')
-    emailAlert.parentElement.classList.add('valid')
-    emailAlert.classList.add('hint')
-  }
-  if (!activitiesValid) {
-    activitiesAlert.parentElement.classList.remove('valid')
-    activitiesAlert.parentElement.classList.add('not-valid')
-    activitiesAlert.classList.remove('hint')
-    e.preventDefault()
-  } else if (activitiesValid) {
-    activitiesAlert.parentElement.classList.remove('not-valid')
-    activitiesAlert.parentElement.classList.add('valid')
-    activitiesAlert.classList.add('hint')
-  }
-})
-
+// set focus to chosen activity and also remove if another checkbox is clicked
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('focus', (e) => {
     const parentFocused = e.target.parentNode
@@ -237,4 +264,28 @@ checkboxes.forEach((checkbox) => {
     const parentBlurred = e.target.parentNode
     parentBlurred.classList.remove('focus')
   })
+})
+
+// event listener for form validation and prevents submit if 1 or more fields is not valid
+form.addEventListener('submit', (e) => {
+  const validChecker = []
+
+  validChecker.push(validName())
+  validChecker.push(validEmail())
+  validChecker.push(validActivities())
+
+  if (payType.value === 'credit-card') {
+    validChecker.push(validCCNumber())
+    validChecker.push(validZipCode())
+    validChecker.push(validCVV())
+  }
+
+  if (validChecker.includes(false)) {
+    e.preventDefault()
+  }
+})
+
+// dynamically check validity of email with keyup event listener
+emailAddress.addEventListener('keyup', () => {
+  validEmail()
 })
